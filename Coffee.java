762e1666a67b8a73ca6cf0f35bfe44be333d8104
skrdus0911 <hgu21200761@gmail.com>
2016-07-04 19:42:39 +0900

    push

--- Coffee.java
+++ Coffee.java
@@ -11,7 +11,7 @@ public abstract class Coffee extends Beverage{
     public abstract void serve();
 
     public int getNumOfSpoons() {
-        return numOfSpoons;
+        return null;
     }
     public void setNumOfSpoons(int numOfSpoons) {
         this.numOfSpoons = numOfSpoons;
